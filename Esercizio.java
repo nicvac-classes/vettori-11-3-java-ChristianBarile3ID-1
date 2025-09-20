95c792312f1ffe7f767e8b01b026e52d4f8ac3bb
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2025-09-20 11:12:17 +0000

    consegna prima parte

--- Esercizio.java
+++ Esercizio.java
@@ -6,20 +6,64 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String args[]) {
+        int N, pos, x, i, scelta, indice;
+        System.out.println("Inserire la grandezza del vettore: ");
+        do {
+            N = Integer.parseInt(in.nextLine());
+        } while (N < 0);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        int[] V = new int[N * 100];
+        azzeraVettore(V, N);
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        do {
+            scelta = leggiComando();
+            switch (scelta) {
+                case 1:
+                    do {
+                        System.out.println("Inserire la posizione: ");
+                        pos = Integer.parseInt(in.nextLine());
+                    } while (pos < 0 || pos > N - 1);
+                    System.out.println("Inserire il valore: ");
+                    x = Integer.parseInt(in.nextLine());
+                    N = inserisciElemento(V, N, pos, x);
+                    break;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+                case 2:
+                    do {
+                        System.out.println("Inserire posizione: ");
+                        pos = Integer.parseInt(in.nextLine());
+                    } while (pos < 0 || pos > N - 1);
+                    N = eliminaElemento(V, N, pos);
+                    break;
+
+                case 3:
+                    System.out.println("Inserire il valore: ");
+                    x = Integer.parseInt(in.nextLine());
+                    indice = ricercaElemento(V, N, x);
+                    if (indice >= 0) {
+                        System.out.println("Elemento: " + x + " trovato in posizione: " + indice);
+                    } else {
+                        System.out.println("Elemento: " + x + " non trovato.");
+                    }
+                    break;
+
+                case 4:
+                    N = eliminaDuplicati(V, N);
+                    break;
+
+                case 5:
+                    visualizzaVettore(V, N);
+                    break;
+
+                case 6:
+                    System.out.println("Uscita dal programma...");
+                    break;
+
+                default:
+                    System.out.println("Opzione non valida.");
+            }
+        } while (scelta != 6);
     }
 }
 
